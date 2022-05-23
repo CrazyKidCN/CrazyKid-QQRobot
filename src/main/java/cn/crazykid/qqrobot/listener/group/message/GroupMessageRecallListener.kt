88c7d1d30efd4b8d1bd15a0b01a915fb69f0740b
CrazyKid <i@crazykid.moe>
2022-05-23 14:49:34 +0800

    功能开关配置化改造

--- src/main/java/cn/crazykid/qqrobot/listener/group/message/GroupMessageRecallListener.kt
+++ src/main/java/cn/crazykid/qqrobot/listener/group/message/GroupMessageRecallListener.kt
@@ -5,7 +5,10 @@ import cc.moecraft.icq.event.IcqListener
 import cc.moecraft.icq.event.events.notice.EventNoticeGroupRecall
 import cc.moecraft.icq.sender.message.MessageBuilder
 import cc.moecraft.icq.sender.message.components.ComponentAt
+import cn.crazykid.qqrobot.enum.FeatureEnum
+import cn.crazykid.qqrobot.service.IFeatureService
 import cn.hutool.core.util.RandomUtil
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 
@@ -20,13 +23,21 @@ class GroupMessageRecallListener : IcqListener() {
     @Value("\${group.recall.mute:false}")
     private var isEnable: Boolean = false
 
+    @Autowired
+    private lateinit var featureService: IFeatureService
+
     @EventHandler
     fun groupMessageRecall(event: EventNoticeGroupRecall) {
         event.getBot().getLogger().debug(
             "群 {} 的 {} 撤回了 {} 的消息: {}", event.getGroupId(),
             event.getOperatorId(), event.getUserId(), event.getMessage(true)
         )
+
         if (!isEnable) {
+            return
+        }
+
+        if (!featureService.isFeatureEnable(event.groupId, FeatureEnum.RECALL_MUTE)) {
             return
         }
 
