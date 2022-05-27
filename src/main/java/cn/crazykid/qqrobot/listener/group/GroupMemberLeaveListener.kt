17fe3bbd170f97dbd4e0d432e82758f862651f3d
CrazyKid <i@crazykid.moe>
2022-05-27 17:40:57 +0800

    功能开关配置化改造

--- src/main/java/cn/crazykid/qqrobot/listener/group/GroupMemberLeaveListener.kt
+++ src/main/java/cn/crazykid/qqrobot/listener/group/GroupMemberLeaveListener.kt
@@ -4,6 +4,9 @@ import cc.moecraft.icq.event.EventHandler
 import cc.moecraft.icq.event.IcqListener
 import cc.moecraft.icq.event.events.notice.groupmember.decrease.EventNoticeGroupMemberLeave
 import cc.moecraft.icq.sender.message.MessageBuilder
+import cn.crazykid.qqrobot.enum.FeatureEnum
+import cn.crazykid.qqrobot.service.IFeatureService
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 
 /**
@@ -14,8 +17,15 @@ import org.springframework.stereotype.Component
  */
 @Component
 class GroupMemberLeaveListener : IcqListener() {
+    @Autowired
+    private lateinit var featureService: IFeatureService
+
     @EventHandler
     fun onEvent(event: EventNoticeGroupMemberLeave) {
+        if (!featureService.isFeatureEnable(event.groupId, FeatureEnum.LEAVE_GROUP_NOTICE)) {
+            return
+        }
+
         val messageBuilder = MessageBuilder()
         messageBuilder.add(event.userId).add(" 退群了")
         event.httpApi.sendGroupMsg(event.groupId, messageBuilder.toString())
