f10942d93cf74a5e6eb577a354e4eaddf68e5a93
CrazyKid <i@crazykid.moe>
2022-07-21 09:20:52 +0800

    启动时拉取水鱼歌曲数据和xray歌曲别名数据改成异步

--- src/main/java/cn/crazykid/qqrobot/service/impl/MaimaiMusicDataServiceImpl.kt
+++ src/main/java/cn/crazykid/qqrobot/service/impl/MaimaiMusicDataServiceImpl.kt
@@ -6,6 +6,7 @@ import cn.crazykid.qqrobot.util.PathUtil
 import cn.hutool.core.io.FileUtil
 import cn.hutool.core.io.resource.ResourceUtil
 import cn.hutool.core.text.csv.CsvUtil
+import cn.hutool.core.thread.ThreadUtil
 import cn.hutool.core.util.StrUtil
 import cn.hutool.http.HttpUtil
 import com.alibaba.fastjson.JSON
@@ -40,30 +41,50 @@ class MaimaiMusicDataServiceImpl : IMaimaiMusicDataService {
 
     @PostConstruct
     fun init() {
-        // read diving fish maimai music data
+        // 读取水鱼查分器歌曲数据
         val divingFishMaimaiMusicDataPath = PathUtil.getClassPath() + "data/" + DIVING_FISH_MAIMAI_MUSIC_DATA_FILE_NAME;
-        try {
-            HttpUtil.downloadFileFromUrl(divingFishApi, divingFishMaimaiMusicDataPath)
-            println("divingFishMaimaiMusicData Update Success")
-        } catch (ignored: Exception) {
-            // ignored
+        val updateDivingFishMaimaiMusicDataTask: Runnable = Runnable {
+            try {
+                // 拉取数据存储本地
+                HttpUtil.downloadFileFromUrl(divingFishApi, divingFishMaimaiMusicDataPath)
+                println("divingFishMaimaiMusicData Update Success")
+            } catch (ignored: Exception) {
+                // ignored
+            }
+        }
+        if (FileUtil.exist(divingFishMaimaiMusicDataPath)) {
+            // 如果此前已拉取过, 则异步执行更新, 避免初始化卡进程
+            ThreadUtil.execute(updateDivingFishMaimaiMusicDataTask);
+        } else {
+            // 没拉取过则同步先拉取
+            updateDivingFishMaimaiMusicDataTask.run();
         }
         val dfjsonStr = FileUtil.readUtf8String(divingFishMaimaiMusicDataPath)
         DIVING_FISH_MAIMAI_MUSIC_DATA = JSON.parseArray(dfjsonStr, DivingFishMaimaiMusicData::class.java)
 
-        // read xray maimai music alias
+        // 读取 xray bot maimai 歌曲别名数据
         val xrayMaimaiMusicAliasPath = PathUtil.getClassPath() + "data/" + XRAY_MAIMAI_MUSIC_ALIAS_FILE_NAME;
-        try {
-            HttpUtil.downloadFileFromUrl(xrayAliasApi, xrayMaimaiMusicAliasPath)
-            println("xrayMaimaiMusicAlias Update Success")
-        } catch (ignored: Exception) {
-            // ignored
+        val updateXrayMaimaiMusicAliasTask: Runnable = Runnable {
+            try {
+                // 拉取数据存储本地
+                HttpUtil.downloadFileFromUrl(xrayAliasApi, xrayMaimaiMusicAliasPath)
+                println("xrayMaimaiMusicAlias Update Success")
+            } catch (ignored: Exception) {
+                // ignored
+            }
+        }
+        if (FileUtil.exist(divingFishMaimaiMusicDataPath)) {
+            // 如果此前已拉取过, 则异步执行更新, 避免初始化卡进程
+            ThreadUtil.execute(updateXrayMaimaiMusicAliasTask);
+        } else {
+            // 没拉取过则同步先拉取
+            updateXrayMaimaiMusicAliasTask.run();
         }
         val xrayjsonStr = FileUtil.readUtf8String(xrayMaimaiMusicAliasPath)
         XRAY_MAIMAI_MUSIC_ALIAS = JSON.parseObject(xrayjsonStr)
 
 
-        // read local aliases.csv file
+        // 读取本地 aliases.csv 别名文件
         val reader = CsvUtil.getReader()
         val data = reader.readFromStr(ResourceUtil.readUtf8Str("aliases.csv"))
         val rows = data.rows
@@ -85,7 +106,7 @@ class MaimaiMusicDataServiceImpl : IMaimaiMusicDataService {
             }
         }
 
-        // merge xray's music alias into the map
+        // 本地歌曲别名数据与 xray 的别名数据合并
         XRAY_MAIMAI_MUSIC_ALIAS?.run {
             forEach { aliasName, musicIdList ->
                 val list = musicIdList as JSONArray
